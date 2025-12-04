c33f8b23dc9b73bd0e7f945b77659c1a8054d99f
IrynaKurza <s30101@pjwstk.edu.pl>
2025-12-04 12:38:45 +0100

    small adjustments

--- ABDULLAgram/Support/Stickerpack.cs
+++ ABDULLAgram/Support/Stickerpack.cs
@@ -19,14 +19,10 @@ namespace ABDULLAgram.Support
         public bool IsPremium { get; set; }
 
         // ==== BASIC ASSOCIATION ====
-        // This stickerpack knows which users saved it
         private HashSet<User> _savedByUsers = new HashSet<User>();
-        
-        // Public getter returns read-only view
         public IReadOnlyCollection<User> SavedByUsers => _savedByUsers.ToList().AsReadOnly();
 
         // ==== PUBLIC METHOD: Called from outside ====
-        // Use this when you want to add a user from the Stickerpack side
         public void AddSavedByUser(User user)
         {
             if (user == null)
@@ -35,10 +31,7 @@ namespace ABDULLAgram.Support
             if (_savedByUsers.Contains(user))
                 throw new InvalidOperationException("This user has already saved this stickerpack.");
 
-            // Add user to our collection
             _savedByUsers.Add(user);
-
-            // REVERSE CONNECTION: Tell the user about this pack
             user.AddStickerpackInternal(this);
         }
 
@@ -51,15 +44,11 @@ namespace ABDULLAgram.Support
             if (!_savedByUsers.Contains(user))
                 throw new InvalidOperationException("This user has not saved this stickerpack.");
 
-            // Remove from our collection
             _savedByUsers.Remove(user);
-
-            // REVERSE CONNECTION: Tell the user to forget this pack
             user.RemoveStickerpackInternal(this);
         }
 
         // ==== INTERNAL METHODS: Called by User class only ====
-        // These just update the collection - no validation, no callbacks
         internal void AddSavedByUserInternal(User user)
         {
             _savedByUsers.Add(user);
